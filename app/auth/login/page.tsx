3c23742f97e21297ff53945c3fcf14bd311406ce
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-31 22:51:53 -0700

    add forgot password button

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -16,42 +16,8 @@ export default async function LogInPage() {
 
   return (
     <>
-      <div className="flex flex-col space-y-2 mb-3 ">
-        <h1 className="text-3xl font-semibold tracking-tight">
-        Welcome Back.
-        </h1>
-        <p className="text-sm text-muted-foreground">
-        Login to continue.
-        </p>
-      </div>
       <UserSignInForm />
-      <p className="px-8 text-center text-sm text-muted-foreground">
-        Don&apos;t have an account?{" "} 
-        <Link
-          href="/auth/signup"
-          className="underline underline-offset-4 hover:text-primary"
-          as="/auth/signup"
-        >
-          Sign up Now!
-        </Link>
-      </p>
-      <p className="px-6 text-center text-xs text-muted-foreground">
-        By clicking continue, you agree to our{" "}
-        <Link
-          href="/terms"
-          className="underline underline-offset-4 hover:text-primary"
-        >
-          Terms of Service
-        </Link>{" "}
-        and{" "}
-        <Link
-          href="/privacy"
-          className="underline underline-offset-4 hover:text-primary"
-        >
-          Privacy Policy
-        </Link>
-        .
-      </p>
+      <p id="below-form-p"><a href="/forgot-password">Forgot your password?</a> We all do.</p>
     </>
   );
 }
