6283e89e28c411b20bca122bb9edf369caaa0e16
Mohammad Rad <argo.mohrad@gmail.com>
2024-05-31 23:05:42 -0700

    add copy right

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,13 +7,12 @@ export default function Footer() {
       <div id="push"></div>
       <div id="footer">
         <div id="inner-footer">
-          {/* Replace this with the actual content of includes/copyright-p.html */}
+          <p id="copyright">&copy; 2011 <strong>Gumroad</strong></p>
           <p>© {new Date().getFullYear()} Gumroad, Inc. All Rights Reserved.</p>
           <p id="footer-navigation">
             <a href="/about">About</a> &bull; <a href="/faq">FAQ</a> &bull; 
             <a href="http://twitter.com/gumroad/">Twitter</a> &bull; 
             <a href="http://facebook.com/gumroad/">Facebook</a>
-            {/* Uncomment if you want to add the Help link */}
             {/* &bull; <a href="#">Help</a> */}
           </p>
         </div>
