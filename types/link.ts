537347f9cbb72348b33ced6e4edfc637af0e2759
Mohammad Rad <argo.mohrad@gmail.com>
2024-06-01 14:29:55 -0700

    add link type

---
+++ types/link.ts
@@ -0,0 +1,13 @@
+export interface LinkType {
+    id: string;
+    owner: string;
+    name: string;
+    price: string;
+    url: string;
+    preview_url: string;
+    description: string;
+    unique_permalink: string;
+    created_at: string;
+    updated_at: string;
+    formatted_price?: string;
+  }
