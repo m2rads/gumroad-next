2c8d0244239b5e6fb7838f135d177c343ed089e6
Mohammad Rad <argo.mohrad@gmail.com>
2024-06-01 14:30:24 -0700

    add link fetching from db

--- components/links.tsx
+++ components/links.tsx
@@ -1,26 +1,49 @@
-'use client'
+'use client';
+
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-// import { supabase } from '../utils/supabase';
+import { createSupabaseBrowserClient } from '@/supabase/client';
+import { useAuth } from './context/auth-context';
+import { LinkType } from '@/types/link';
+
 
 const LinksComponent: React.FC = () => {
-  const [links, setLinks] = useState([]);
+  const [links, setLinks] = useState<LinkType[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+  const { user } = useAuth();
+  const supabase = createSupabaseBrowserClient();
 
   useEffect(() => {
     const fetchLinks = async () => {
-    //   const { data, error } = await supabase.from('links').select('*');
-    //   if (error) {
-    //     setError(error.message);
-    //   } else {
-    //     setLinks(data);
-    //   }
-      setLoading(false);
+      if (!user) return;
+
+      try {
+        const { data: linksData, error } = await supabase
+          .from('links')
+          .select('*')
+          .eq('owner', user.id);
+
+        if (error) {
+          setError(error.message);
+          return;
+        }
+
+        const formattedLinks = linksData.map((link: LinkType) => ({
+          ...link,
+          formatted_price: parseFloat(link.price).toFixed(2),
+        }));
+
+        setLinks(formattedLinks);
+      } catch (error) {
+        setError((error as Error).message);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchLinks();
-  }, []);
+  }, [user]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -30,17 +53,29 @@ const LinksComponent: React.FC = () => {
     return <div>Error: {error}</div>;
   }
 
+  const linksMessage =
+    links.length === 0
+      ? 'create one, you know you want to'
+      : links.length < 3
+      ? 'not too bad...'
+      : "that's a lot!";
+
   return (
     <div id="dashboard" className="links-page">
-      <h3>{links.length} link{links.length !== 1 && 's'} <small>{/* links_message */}</small></h3>
-      <Link href="/add" className="button" id="add-link-button">Add link</Link>
+      <h3>
+        {links.length} link{links.length !== 1 && 's'} <small>{linksMessage}</small>
+      </h3>
+      <Link href="/add" className="button" id="add-link-button">
+        Add link
+      </Link>
       {links.length > 0 && (
         <>
           <div className="mini-rule"></div>
           <ul id="links">
-            {links.map((link: any) => (
+            {links.map((link) => (
               <li key={link.id}>
-                <Link href={`/edit/${link.unique_permalink}`}>{link.name}</Link> <small>${link.formatted_price}</small>
+                <Link href={`/edit/${link.unique_permalink}`}>{link.name}</Link>{' '}
+                <small>${link.formatted_price}</small>
               </li>
             ))}
           </ul>
