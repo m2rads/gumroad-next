bd8931861c80c708b9610f8d51607046037240a5
Mohammad Rad <argo.mohrad@gmail.com>
2024-06-02 03:36:18 -0700

    escape special characters

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div id="main-content">
 		<h3>404</h3>
 		<div className="mini-rule"></div>
-		<p>You&apos;ve reached a page that does&apos;'t exist. <Link href="/home">Go home?</Link></p>
+		<p>You&apos;ve reached a page that does&apos;t exist. <Link href="/home">Go home?</Link></p>
 	</div>
   )
 }
